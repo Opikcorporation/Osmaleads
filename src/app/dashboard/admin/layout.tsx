83f96fa88e050a58c015bd606b2934e2e9964450
Alessio <aless.opik@gmail.com>
2025-11-10 13:24:54 +0000

    la bar nav s'affiche 2x par moment

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -1,9 +1,11 @@
-import DashboardLayout from "@/app/dashboard/layout";
 
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <DashboardLayout>{children}</DashboardLayout>;
+  // Ce layout est un simple passe-plat.
+  // Next.js imbrique automatiquement ce contenu dans le `dashboard/layout.tsx` parent.
+  // Il n'est pas nécessaire de ré-envelopper les enfants dans DashboardLayout ici.
+  return <>{children}</>;
 }
